8b495023c265bb6b155a9475d1b306e4638c85d9
LiamMesarec <mesarec.liam@gmail.com>
2023-04-16 22:22:32 +0200

    NOV-25 bugfix test5

--- server/config/scrapers/gov.ts
+++ server/config/scrapers/gov.ts
@@ -1,34 +1,34 @@
-import cheerio from 'cheerio'
-import axios from 'axios'
+import cheerio from 'cheerio';
+import axios from 'axios';
 
 async function _gov(n: number) {
-    axios.get('https://www.gov.si/novice/')
-          .then(response => {
-            const $ = cheerio.load(response.data);
-            const titles: string[] = [];
-            const urls: string[] = [];
+  axios
+    .get('https://www.gov.si/novice/')
+    .then((response) => {
+      const $ = cheerio.load(response.data);
+      const titles: string[] = [];
+      const urls: string[] = [];
 
-            $('.title').each((i, element) => {
-              const title: string = $(element).text();
-              const url: string | undefined = $(element).find('a').attr('href');
-              if (url) {
-                urls.push("https://www.gov.si" + url);
-              }
-              titles.push(title.trim());
-                if(i == n - 1) {
-                    return false;
-                }
-            });
+      $('.title').each((i, element) => {
+        const title: string = $(element).text();
+        const url: string | undefined = $(element).find('a').attr('href');
+        if (url) {
+          urls.push('https://www.gov.si' + url);
+        }
+        titles.push(title.trim());
+        if (i == n - 1) {
+          return false;
+        }
+      });
 
-            console.log('Titles:', titles);
-            console.log('URLs:', urls);
-          })
-          .catch(error => {
-            console.log(error);
-          });
+      console.log('Titles:', titles);
+      console.log('URLs:', urls);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 
-        return ""
-
+  return '';
 }
 
 export = _gov;
