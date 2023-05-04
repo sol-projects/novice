2d4f2f0ce6a8aee57c784729867a34ac045fd7a5
Senad Salkanovic <senad.salkanovic14@gmail.com>
2023-05-05 00:13:45 +0300

    NOV-65
    Dodajanje screjpanje za mariborinfo.com

--- server/scraper/websites.ts
+++ server/scraper/websites.ts
@@ -1,9 +1,10 @@
 import { INews } from '../model/News';
 
 export const websites = new Map<string, (n: number) => Promise<INews[]>>([
-  ['gov novice', require('./websites/gov')],
+  //['gov novice', require('./websites/gov')],
   //['gov novice vlade', require('./websites/gov_vlada')],
-  ['24 ur', require('./websites/24ur')],
+  //['24 ur', require('./websites/24ur')],
   //['siol', require('./websites/siol')],
   //['delo', require('./websites/delo')],
+  ['mbinfo', require('./websites/mariborinfo')],
 ]);
