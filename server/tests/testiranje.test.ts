017b2cad87ab2714a0d26dc6903800d1950bb19b
Senad Salkanovic <senad.salkanovic14@gmail.com>
2023-05-09 13:32:33 +0300

    NOV-82(testiranje update)

--- server/tests/testiranje.test.ts
+++ server/tests/testiranje.test.ts
@@ -2,9 +2,8 @@ import mongoose from 'mongoose';
 import request from 'supertest';
 import { describe, expect, test, beforeAll, afterAll } from '@jest/globals';
 import app from '../index';
-const DB_URI = "mongodb+srv://senad:senad@cluster0.bd0tfwp.mongodb.net/?retryWrites=true&w=majority";
 
-import { INews } from '../model/News';
+import { INews, News } from '../model/News';
 
 test('GET /news - should return all news', async () => {
   const res = await request(app).get('/news');
@@ -24,28 +23,6 @@ test('GET /news - should return all news', async () => {
     expect(newsItem).toHaveProperty('location');
   });
 },50000);
-
-test('GET /news/scrape/:n - should scrape a specific number of websites and return news objects', async () => {
-  const numberOfWebsites = 3;
-  const res = await request(app).get(`/news/scrape/${numberOfWebsites}`);
-  expect(res.status).toEqual(200);
-  expect(res.headers['content-type']).toMatch(/json/);
-  expect(Array.isArray(res.body)).toBeTruthy();
-
-  // Check if the response data contains the expected properties
-  res.body.forEach((newsItem: any) => {
-    expect(newsItem).toHaveProperty('title');
-    expect(newsItem).toHaveProperty('url');
-    expect(newsItem).toHaveProperty('date');
-    expect(newsItem).toHaveProperty('authors');
-    expect(newsItem).toHaveProperty('content');
-    expect(newsItem).toHaveProperty('categories');
-    expect(newsItem).toHaveProperty('location');
-  });
-
-  // Check if the returned objects are greater than or equal to the number of websites
-  expect(res.body.length).toBeGreaterThanOrEqual(numberOfWebsites);
-}, 50000); // Set timeout to 50,000ms to accommodate scraping time
 
 test('GET /news - should get 5 news items and check categories and their values', async () => {
   const res = await request(app).get('/news?limit=5');
@@ -71,7 +48,39 @@ test('GET /news - should get 5 news items and check categories and their values'
       expect(category.trim()).not.toEqual('');
     });
   });
-
-  // Check if the returned objects are equal to the specified limit
-  expect(res.body.length).toEqual(5);
 });
+
+describe('POST and DELETE news', () => {
+  let savedNewsId: string;
+  const testNews = {
+    title: 'Test News',
+    content: 'This is a test news article.',
+    authors: ['Test Author'],
+    categories: ['Test Category'],
+    location: 'Test Location',
+  };
+
+  test('POST news', async () => {
+    const response = await request(app)
+      .post('/news')
+      .send(testNews)
+      .expect(201);
+
+    expect(response.body.title).toEqual(testNews.title);
+    expect(response.body.content).toEqual(testNews.content);
+    expect(response.body.authors).toEqual(testNews.authors);
+    expect(response.body.categories).toEqual(testNews.categories);
+    expect(response.body.location).toEqual(testNews.location);
+    savedNewsId = response.body._id;
+  });
+
+  test('DELETE news', async () => {
+    await request(app).delete(`/news/${savedNewsId}`).expect(200);
+
+    const deletedNews = await News.findById(savedNewsId);
+    expect(deletedNews).toBeNull();
+  });
+});
+
+
+
