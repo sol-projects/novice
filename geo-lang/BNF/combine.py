44c2268698ecaf61396c732e7c2d8f5597ba633a
LiamMesarec <mesarec.liam@gmail.com>
2023-05-10 16:36:57 +0200

    NOV-51 combining script, bnf files

---
+++ geo-lang/BNF/combine.py
@@ -0,0 +1,16 @@
+import glob
+
+def combine_bnf_files():
+    output_file = "combined_bnf.bnf"
+    file_list = glob.glob("*.bnf")
+
+    with open(output_file, 'w') as outfile:
+        for filename in file_list:
+            with open(filename, 'r') as infile:
+                for line in infile:
+                    if not line.startswith('@'):
+                        outfile.write(line)
+
+    print(f"Combined BNF files into: {output_file}")
+
+combine_bnf_files()
