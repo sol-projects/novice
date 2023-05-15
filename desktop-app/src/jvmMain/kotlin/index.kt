40d269c0540694af6455c807f14eda9ad9fb7ee8
LiamMesarec <mesarec.liam@gmail.com>
2023-05-15 15:57:31 +0200

    Revert "NOV-22 gui framework"
    This reverts commit 3778e1dbadd16c1d6fdabd2878efdaa51732ffd9.

--- desktop-app/src/jvmMain/kotlin/index.kt
+++ desktop-app/src/jvmMain/kotlin/index.kt
@@ -1,19 +1,12 @@
 package org.example
-import App
 import _24ur
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
 import getRtvSlo
 import getZurnal24Slo
 import getServisSta
 import org.example.scraper.gov
 import org.example.scraper.gov_vlada
 
-fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
-    }
-
+fun main() {
     _24ur(2)
     println(gov(2))
     println(gov_vlada(2))
