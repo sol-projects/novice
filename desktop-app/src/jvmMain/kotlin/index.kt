9c072d02cdccab9e9bafcf1ef3e5c30a27958a5d
LiamMesarec <mesarec.liam@gmail.com>
2023-05-15 16:08:51 +0200

    NOV-22 gui framework 2

--- desktop-app/src/jvmMain/kotlin/index.kt
+++ desktop-app/src/jvmMain/kotlin/index.kt
@@ -1,16 +1,20 @@
 package org.example
+import App
 import _24ur
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
 import getRtvSlo
 import getZurnal24Slo
 import getServisSta
 import org.example.scraper.gov
 import org.example.scraper.gov_vlada
 
-fun main() {
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication) {
+        App()
+    }
+
     _24ur(2)
     println(gov(2))
     println(gov_vlada(2))
-    getRtvSlo(2)
-    getServisSta(2)
-    getZurnal24Slo(2)
 }
