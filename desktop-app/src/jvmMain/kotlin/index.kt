3b993aca752125b80f9046744670378df21d1e5c
ProgramingGangstar <ognjen.vuckovic@student.um.si>
2023-05-15 15:32:14 +0200

    NOV-71 fix

---
+++ desktop-app/src/jvmMain/kotlin/index.kt
@@ -0,0 +1,16 @@
+package org.example
+import _24ur
+import getRtvSlo
+import getZurnal24Slo
+import getServisSta
+import org.example.scraper.gov
+import org.example.scraper.gov_vlada
+
+fun main() {
+    _24ur(2)
+    println(gov(2))
+    println(gov_vlada(2))
+    getRtvSlo(2)
+    getServisSta(2)
+    getZurnal24Slo(2)
+}
