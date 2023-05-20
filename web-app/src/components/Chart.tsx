78857572f4339e180a0db2c05b522fdba4df06d4
LiamMesarec <mesarec.liam@gmail.com>
2023-05-20 19:00:40 +0200

    NOV-29 charts for date/category aggregation

--- web-app/src/components/Chart.tsx
+++ web-app/src/components/Chart.tsx
@@ -1,14 +1,54 @@
 import React from "react";
-import { VStack, Center } from "@chakra-ui/react";
+import {
+  VStack,
+  Center,
+  RadioGroup,
+  Radio,
+  Stack,
+  Heading,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import NewsArticle from "./NewsArticle";
 import INews from "../news/model";
-import { ResponsiveLine } from "@nivo/line";
+import { ResponsiveBar } from "@nivo/bar";
 import { getAll } from "../news/api";
+import Filter, { FilterData } from "./Filter";
+import * as FilterFn from "../news/filter";
+import * as Aggregate from "../news/aggregate";
+
+type ChartInfo = {
+  bottom: string;
+  left: string;
+  dataAmount: number;
+};
+
+let chartInfo: ChartInfo = {
+  bottom: "categories",
+  left: "number of articles",
+  dataAmount: 10,
+};
+
+function dataToDisplay(option: string, news: INews[], n: number) {
+  switch (option) {
+    case "aggrCategory":
+      chartInfo.bottom = "categories";
+      chartInfo.left = "number of articles";
+      return Aggregate.topCategories(news, n);
+    case "aggrDate":
+      chartInfo.bottom = "dates";
+      chartInfo.left = "number of articles";
+      return Aggregate.byDate(news, n);
+    default:
+      return Aggregate.topCategories(news, n);
+  }
+}
 
 export default function Chart() {
   const [news, set] = useState<INews[]>([]);
+  const [filteredNews, setFilteredNews] = useState<INews[]>([]);
+  const [value, setValue] = useState("category");
+
   useEffect(() => {
     const get = async () => {
       const data = await getAll();
@@ -25,71 +65,128 @@ export default function Chart() {
     socket.on("news-added", (newNews) => {
       set(newNews);
     });
+    return () => {
+      socket.disconnect();
+    };
   }, []);
 
+  const handleFilterChange = (filterData: FilterData) => {
+    let filtered = news;
+    filterData.categories = filterData.categories.filter((item) => item !== "");
+    filterData.authors = filterData.authors.filter((item) => item !== "");
+    if (filterData.categories.length > 0) {
+      filtered = FilterFn.categories(filtered, filterData.categories);
+    }
+
+    if (filterData.authors.length > 0) {
+      filtered = FilterFn.authors(filtered, filterData.authors);
+    }
+
+    setFilteredNews(filtered);
+  };
+
   return (
-    <ResponsiveLine
-      data={[]}
-      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-      xScale={{ type: "point" }}
-      yScale={{
-        type: "linear",
-        min: "auto",
-        max: "auto",
-        stacked: true,
-        reverse: false,
-      }}
-      yFormat=" >-.2f"
-      axisTop={null}
-      axisRight={null}
-      axisBottom={{
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: "transportation",
-        legendOffset: 36,
-        legendPosition: "middle",
-      }}
-      axisLeft={{
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: "count",
-        legendOffset: -40,
-        legendPosition: "middle",
-      }}
-      pointSize={10}
-      pointColor={{ theme: "background" }}
-      pointBorderWidth={2}
-      pointBorderColor={{ from: "serieColor" }}
-      pointLabelYOffset={-12}
-      useMesh={true}
-      legends={[
-        {
-          anchor: "bottom-right",
-          direction: "column",
-          justify: false,
-          translateX: 100,
-          translateY: 0,
-          itemsSpacing: 0,
-          itemDirection: "left-to-right",
-          itemWidth: 80,
-          itemHeight: 20,
-          itemOpacity: 0.75,
-          symbolSize: 12,
-          symbolShape: "circle",
-          symbolBorderColor: "rgba(0, 0, 0, .5)",
-          effects: [
+    <Center>
+      <VStack width="80%" style={{ height: 600 }}>
+        <Filter onChange={handleFilterChange} />
+        <RadioGroup onChange={setValue} value={value}>
+          <Stack direction="row">
+            <Heading as="h4" size="md">
+              agregacija:
+            </Heading>
+            <Radio value="aggrCategory">kategorije</Radio>
+            <Radio value="aggrDate">datum</Radio>
+          </Stack>
+        </RadioGroup>
+        <ResponsiveBar
+          data={dataToDisplay(value, filteredNews, chartInfo.dataAmount)}
+          keys={["value"]}
+          indexBy="key"
+          margin={{ top: 50, right: 200, bottom: 100, left: 100 }}
+          padding={0.3}
+          valueScale={{ type: "linear" }}
+          indexScale={{ type: "band", round: true }}
+          colors={{ scheme: "nivo" }}
+          defs={[
             {
-              on: "hover",
-              style: {
-                itemBackground: "rgba(0, 0, 0, .03)",
-                itemOpacity: 1,
-              },
+              id: "dots",
+              type: "patternDots",
+              background: "inherit",
+              color: "#38bcb2",
+              size: 4,
+              padding: 1,
+              stagger: true,
             },
-          ],
-        },
-      ]}
-    />
+            {
+              id: "lines",
+              type: "patternLines",
+              background: "inherit",
+              color: "#eed312",
+              rotation: -45,
+              lineWidth: 6,
+              spacing: 10,
+            },
+          ]}
+          borderColor={{
+            from: "color",
+            modifiers: [["darker", 1.6]],
+          }}
+          axisTop={null}
+          axisRight={null}
+          axisBottom={{
+            format: (v) => {
+              return v.length > 10 ? `${v.substring(0, 10)}...` : v;
+            },
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: chartInfo.bottom,
+            legendPosition: "middle",
+            legendOffset: 32,
+          }}
+          axisLeft={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: chartInfo.left,
+            legendPosition: "middle",
+            legendOffset: -40,
+          }}
+          labelSkipWidth={12}
+          labelSkipHeight={12}
+          labelTextColor={{
+            from: "color",
+            modifiers: [["darker", 1.6]],
+          }}
+          /*legends={[
+            {
+                dataFrom: 'indexes',
+                anchor: 'bottom-right',
+                direction: 'column',
+                justify: false,
+                translateX: 120,
+                translateY: 0,
+                itemsSpacing: 2,
+                itemWidth: 100,
+                itemHeight: 20,
+                itemDirection: 'left-to-right',
+                itemOpacity: 0.85,
+                symbolSize: 20,
+                effects: [
+                    {
+                        on: 'hover',
+                        style: {
+                            itemOpacity: 1
+                        }
+                    }
+                ]
+            }
+        ]}*/
+          role="application"
+          //ariaLabel="Nivo bar chart demo"
+          //barAriaLabel={e=>e.id+": "+e.formattedValue+" in country: "+e.indexValue}
+        />
+      </VStack>
+    </Center>
   );
 }
---
+++ web-app/src/news/aggregate.ts
@@ -0,0 +1,38 @@
+import INews from "./model";
+
+export type Aggregation = {
+  key: string;
+  value: number;
+};
+
+export function topCategories(news: INews[], n: number): Aggregation[] {
+  const frequencies: Record<string, number> = {};
+
+  for (const newsItem of news) {
+    for (const category of newsItem.categories) {
+      frequencies[category] = (frequencies[category] || 0) + 1;
+    }
+  }
+
+  return sort(Object.entries(frequencies)).slice(0, n);
+}
+
+export function byDate(news: INews[], n: number): Aggregation[] {
+  const frequencies: Record<string, number> = {};
+
+  for (const newsItem of news) {
+    const dateKey = newsItem.date.toISOString().split("T")[0];
+
+    frequencies[dateKey] = (frequencies[dateKey] || 0) + 1;
+  }
+
+  return sort(Object.entries(frequencies)).slice(0, n);
+}
+
+function sort(entries: [string, number][]): Aggregation[] {
+  entries.sort((a, b) => {
+    return b[1] - a[1];
+  });
+
+  return entries.map(([key, value]) => ({ key, value }));
+}
