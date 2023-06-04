2877ec43de3c91cca00385bcc0ac2a1e4b0e4f08
LiamMesarec <mesarec.liam@gmail.com>
2023-06-04 22:48:05 +0200

    NOV-107 tags

--- web-app/src/components/NewsArticle.tsx
+++ web-app/src/components/NewsArticle.tsx
@@ -8,11 +8,18 @@ import {
   Stack,
   Heading,
   Link,
+  Tag,
+  Flex,
+  Spacer,
+  Wrap,
+  WrapItem,
 } from "@chakra-ui/react";
 
 export default function NewsArticle(props: any) {
   const { article } = props;
   const date = new Date(article.date);
+  const displayedTags = article.categories.slice(0, 4);
+  const remainingTagsCount = article.categories.length - 4;
 
   const addView = async () => {
     console.log("success");
@@ -57,7 +64,36 @@ export default function NewsArticle(props: any) {
           <Text>Avtorji: {article.authors.toString()}</Text>
           <Text>Datum: {date.toLocaleString("sl-SI")}</Text>
           <Text>Ogledi: {article.views.length}</Text>
-          <Text>Kategorije: {article.categories.toString()}</Text>
+          <Wrap mt={2} spacing={2}>
+            {displayedTags.map((tag: string, index: number) => (
+              <WrapItem key={index}>
+                <Tag
+                  size="md"
+                  variant="solid"
+                  colorScheme="teal"
+                  borderRadius="full"
+                  px={3}
+                  py={1}
+                >
+                  {tag}
+                </Tag>
+              </WrapItem>
+            ))}
+            {remainingTagsCount > 0 && (
+              <WrapItem>
+                <Tag
+                  size="md"
+                  variant="solid"
+                  colorScheme="teal"
+                  borderRadius="full"
+                  px={3}
+                  py={1}
+                >
+                  +{remainingTagsCount}
+                </Tag>
+              </WrapItem>
+            )}
+          </Wrap>
         </Stack>
         <Stack width="70%">
           <CardBody>
